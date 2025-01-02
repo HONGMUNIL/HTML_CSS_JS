1afbeddee81f342854fd1434dc519058e61c8542
HONGMUNIL <bear4564@naver.com>
2025-01-02 17:52:13 +0900

    Iphone

--- board/js/clock.js
+++ board/js/clock.js
@@ -1,19 +1,12 @@
-function clockRun(){
+function clockRun() {
     const phoneClock = document.querySelector(".phone-clock");
     setInterval(() => {
         const now = new Date();
-        const noewHours = now.getHours();
-        const noewMinutes = now.getMinutes();
-        const nowClockText = `${noewHours}:${noewMinutes}`;
+        const nowHours = now.getHours();
+        const nowMinutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+        const nowClockText = `${nowHours}:${nowMinutes}`;
         phoneClock.innerHTML = nowClockText;
     }, 1000);
 }
 
 clockRun();
-
-
-
-
-
-
-
