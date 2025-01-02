244b85d36739b5dc4817e9797384cacee32cbee7
MunIL HONG <bear4564@naver.com>
2025-01-02 21:16:04 +0900

    ipad

---
+++ board2/js/clock.js
@@ -0,0 +1,13 @@
+function clockRun(){
+    const ipadClock = document.querySelector(".ipad-clock");
+    setInterval(() => {
+        const now = new Data();
+        const nowHours =now.getHours();
+        const nowMinutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+        const nowClockText = `${nowHours}:${nowMinutes}`;
+        ipadClock.innerHTML = nowClockText;
+    
+    },1000);
+}
+
+clockRun();
